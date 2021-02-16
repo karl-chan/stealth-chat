d9b71f181ef5734709c9f6d201fa26ab078cad12
Karl Chan <karl-chan@bitbucket.org>
2021-02-16 21:17:58 +0000

    Add qr code scanner

--- client/lib/contact/add_contact_page.dart
+++ client/lib/contact/add_contact_page.dart
@@ -1,38 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:share/share.dart';
+import 'package:stealth_chat/contact/accept_invite_page.dart';
 import 'package:stealth_chat/globals.dart';
 import 'package:stealth_chat/util/security/rsa.dart';
 
 class AddContactController extends GetxController {
   final Globals globals;
   final TextEditingController personalMessageController;
-  final Uri inviteLink;
+  final String scheme;
+  final String host;
 
+  Uri inviteLink;
   RxString message = ''.obs;
   RxString renderedMessage = ''.obs;
 
   AddContactController(Globals globals)
       : this.globals = globals,
+        this.scheme = globals.properties.get('applink.scheme'),
+        this.host = globals.properties.get('applink.host'),
         this.personalMessageController =
-            TextEditingController(text: 'Hi, this is ${globals.user.name}!'),
-        this.inviteLink = generateInviteLink(globals) {
+            TextEditingController(text: 'Hi, this is ${globals.user.name}!') {
+    this.inviteLink = generateInviteLink(globals);
     updateRenderedMessage();
     personalMessageController.addListener(updateRenderedMessage);
   }
 
-  static Uri generateInviteLink(Globals globals) {
+  Uri generateInviteLink(Globals globals) {
     String id = globals.user.id;
     String name = globals.user.name;
     String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
     String signature = Rsa.sign('$id|$name|$timestamp', globals.user.keys);
 
     return Uri(
-        scheme: globals.properties.get('applink.scheme'),
-        host: globals.properties.get('applink.host'),
+        scheme: this.scheme,
+        host: this.host,
         path: Paths.ACCEPT_INVITE,
         queryParameters: {
           'id': id,
@@ -53,6 +60,71 @@ $inviteLink''';
   void sendInvite() {
     Share.share(renderedMessage.value,
         subject: 'Invite from ${globals.user.name}');
+  }
+
+  Future<void> showAcceptInviteTextDialog() async {
+    TextEditingController controller = TextEditingController();
+    Rx<Uri> acceptInviteLink = Rx(null);
+    await Get.defaultDialog(
+      title: 'Paste invite link',
+      content: Obx(() => TextField(
+            controller: controller,
+            decoration: InputDecoration(
+                suffix: IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: () async {
+                      final data = await Clipboard.getData('text/plain');
+                      if (data != null) {
+                        controller.text = data.text;
+                      }
+                    }),
+                hintText: 'e.g. app://chat/accept-invite/...',
+                errorText:
+                    acceptInviteLink.value == null && controller.text.isNotEmpty
+                        ? 'Not a valid link!'
+                        : null),
+            onChanged: (input) {
+              final uri = Uri.tryParse(input.trim());
+              if (uri != null &&
+                  uri.scheme == this.scheme &&
+                  uri.host == this.host &&
+                  uri.path == Paths.ACCEPT_INVITE) {
+                acceptInviteLink.value = uri;
+              } else {
+                acceptInviteLink.nil();
+              }
+            },
+          )),
+      textConfirm: 'Enter',
+      confirmTextColor: Colors.white,
+      onConfirm: () async {
+        if (acceptInviteLink.value != null) {
+          await Get.to(AcceptInvitePage(acceptInviteLink.value));
+        }
+      },
+    );
+  }
+
+  Future<void> showAcceptInviteQRDialog() async {
+    await Get.defaultDialog(
+      title: 'Scan QR code',
+      content: SizedBox(
+          width: 200,
+          height: 200,
+          child: QRView(
+              key: GlobalKey(debugLabel: 'AcceptInviteQRDialog'),
+              onQRViewCreated: (controller) {
+                controller.scannedDataStream.listen((scanData) async {
+                  final uri = Uri.tryParse(scanData.code);
+                  if (uri != null &&
+                      uri.scheme == this.scheme &&
+                      uri.host == this.host &&
+                      uri.path == Paths.ACCEPT_INVITE) {
+                    await Get.to(AcceptInvitePage(uri));
+                  }
+                });
+              })),
+    );
   }
 }
 
@@ -142,6 +214,17 @@ class ViaMessageTab extends StatelessWidget {
           color: Colors.green,
           textColor: Colors.white,
           onPressed: c.sendInvite,
+        ),
+        MaterialButton(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('I already have an invite link!'),
+            ],
+          ),
+          color: Colors.orange,
+          textColor: Colors.white,
+          onPressed: c.showAcceptInviteTextDialog,
         )
       ],
     ));
@@ -170,6 +253,19 @@ class InPersonTab extends StatelessWidget {
         QrImage(
           padding: EdgeInsets.all(10),
           data: c.inviteLink.toString(),
+        ),
+        MaterialButton(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('I have a code to scan'),
+              const SizedBox(width: 10),
+              const Icon(Icons.camera_alt)
+            ],
+          ),
+          color: Colors.orange,
+          textColor: Colors.white,
+          onPressed: c.showAcceptInviteQRDialog,
         ),
       ],
     ));
